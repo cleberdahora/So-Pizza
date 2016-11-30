6838d8eb237a474ceacc5e24e55f904bc31028b1
cleberdahora <cleberdahora@hotmail.com>
2016-11-30 08:14:07 -0200

    Update consult.js

--- public/js/consult.js
+++ public/js/consult.js
@@ -26,7 +26,7 @@
             var request = require('superagent');
 
             request
-                    .get('http://sopizzabrasil.herokuapp.com/consult')
+                    .get('https://sopizzabrasil.herokuapp.com/consult')
 
                     .end(function (err, res) {
 
@@ -36,7 +36,7 @@
 
 
             request
-                    .get('http://sopizzabrasil.herokuapp.com/cardap')
+                    .get('https://sopizzabrasil.herokuapp.com/cardap')
 
                     .end(function (err, res) {
 
@@ -59,7 +59,7 @@
 
                 }
                 var id = getUrlVars()["id"];
-                $.get('http://sopizzabrasil.herokuapp.com/consult', function (pontos) {
+                $.get('https://sopizzabrasil.herokuapp.com/consult', function (pontos) {
 
                     var PizzariaID;
                     var PizzariaNome;
